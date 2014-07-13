8dd12c66a935d1902edf254dedc69a3ae48529ee
Ben Formosa <ben.formosa@gmail.com>
2014-07-13 17:14:06 +1000

    file choosing

--- task1/InterestMap.java
+++ task1/InterestMap.java
@@ -1,10 +1,30 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class InterestMap extends JFrame {
+public class InterestMap extends JFrame implements ActionListener {
+  JMenuItem menuLoadPOI;
+  JFileChooser fc;
+
   public InterestMap() {
     JPanel panel = new JPanel();
+
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menuFile = new JMenu("File");
+    menuFile.setMnemonic(KeyEvent.VK_F);
+
+    menuLoadPOI = new JMenuItem("Load Points of Interest", KeyEvent.VK_P);
+    menuLoadPOI.addActionListener(this);
+    menuFile.add(menuLoadPOI);
+    menuBar.add(menuFile);
+
+    fc = new JFileChooser();
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("POI Files", "poi", "csv");
+    fc.setFileFilter(filter);
+
+    this.setJMenuBar(menuBar);
     this.add(panel, BorderLayout.SOUTH);
   }
 
@@ -14,18 +34,21 @@ public class InterestMap extends JFrame {
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JMenuBar menuBar = new JMenuBar();
-    JMenu menuFile = new JMenu("File");
-    menuFile.setMnemonic(KeyEvent.VK_F);
-
-    JMenuItem menuLoadPOI = new JMenuItem("Load Points of Interest", KeyEvent.VK_P);
-    menuLoadPOI.addActionListener(this);
-    menuFile.add(menuLoadPOI);
-
-    menuBar.add(menuFile);
-    frame.setJMenuBar(menuBar);
-
     frame.setSize(600, 400);
     frame.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == menuLoadPOI) {
+
+      int returnVal = fc.showOpenDialog(InterestMap.this);
+
+      if (returnVal == JFileChooser.APPROVE_OPTION) {
+        File file = fc.getSelectedFile();
+        System.out.println(file.getName());
+      } else {
+        System.out.println("File access cancelled by user.");
+      }
+    }
   }
 }
