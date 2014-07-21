91e129e9815b72a6b490101165d0555db5324bc1
Ben Formosa <ben.formosa@gmail.com>
2014-07-21 21:32:04 +1000

    zoom and pan

--- task1/src/itc313_ass1/InterestMap.java
+++ task1/src/itc313_ass1/InterestMap.java
@@ -10,17 +10,34 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class InterestMap extends JFrame implements ActionListener {
   JMenuItem menuLoadPOI;
   JFileChooser fc;
+  MapPanel mapPanel;
+  JPanel toolBar;
+  JButton panNButton, panSButton, panEButton, panWButton, zoomInButton, zoomOutButton;
 
   public InterestMap() {
     try {
-      JPanel panel = new MapPanel("1.poi");
+      mapPanel = new MapPanel("1.poi");
+      toolBar = new JPanel();
 
       JMenuBar menuBar = new JMenuBar();
       JMenu menuFile = new JMenu("File");
       menuFile.setMnemonic(KeyEvent.VK_F);
 
       menuLoadPOI = new JMenuItem("Load Points of Interest", KeyEvent.VK_P);
-      menuLoadPOI.addActionListener(this);
+      menuLoadPOI.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          int returnVal = fc.showOpenDialog(InterestMap.this);
+
+          if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            System.out.println(file.getName());
+          } else {
+            System.out.println("File access cancelled by user.");
+          }
+        }
+      });
+
       menuFile.add(menuLoadPOI);
       menuBar.add(menuFile);
 
@@ -28,8 +45,31 @@ public class InterestMap extends JFrame implements ActionListener {
       FileNameExtensionFilter filter = new FileNameExtensionFilter("POI Files", "poi", "csv");
       fc.setFileFilter(filter);
 
+      panNButton = new JButton("↑");
+      panSButton = new JButton("↓");
+      panEButton = new JButton("→");
+      panWButton = new JButton("←");
+      zoomInButton = new JButton("+");
+      zoomOutButton = new JButton("-");
+      panNButton.addActionListener(this);
+      panSButton.addActionListener(this);
+      panEButton.addActionListener(this);
+      panWButton.addActionListener(this);
+      zoomInButton.addActionListener(this);
+      zoomOutButton.addActionListener(this);
+
+      toolBar.add(panNButton);
+      toolBar.add(panSButton);
+      toolBar.add(panEButton);
+      toolBar.add(panWButton);
+      toolBar.add(zoomInButton);
+      toolBar.add(zoomOutButton);
+
       this.setJMenuBar(menuBar);
-      this.add(panel);
+      this.add(toolBar, BorderLayout.NORTH);
+      this.add(mapPanel, BorderLayout.CENTER);
+
+      this.pack();
     } catch(IOException ex){
     }
   }
@@ -44,17 +84,21 @@ public class InterestMap extends JFrame implements ActionListener {
     frame.setVisible(true);
   }
 
+  @Override
   public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == menuLoadPOI) {
-
-      int returnVal = fc.showOpenDialog(InterestMap.this);
-
-      if (returnVal == JFileChooser.APPROVE_OPTION) {
-        File file = fc.getSelectedFile();
-        System.out.println(file.getName());
-      } else {
-        System.out.println("File access cancelled by user.");
-      }
+    if(e.getSource() == panNButton) {
+      mapPanel.panN();
+    } else if(e.getSource() == panSButton) {
+      mapPanel.panS();
+    } else if(e.getSource() == panEButton) {
+      mapPanel.panE();
+    } else if(e.getSource() == panWButton) {
+      mapPanel.panW();
+    } else if(e.getSource() == zoomInButton) {
+      mapPanel.zoomIn();
+    } else if(e.getSource() == zoomOutButton) {
+      mapPanel.zoomOut();
     }
+    mapPanel.repaint();
   }
 }
--- task1/src/itc313_ass1/MapPanel.java
+++ task1/src/itc313_ass1/MapPanel.java
@@ -21,10 +21,14 @@ public class MapPanel extends JPanel {
   //implement zoom and pan based on scale and origin
   //wrap the view around like asteroids
 
-  int scale = 1080; /* scale is number of seconds per pixel */
+  private static int PAN_DISTANCE = 1; // pan this many arcseconds by default
+  private static int ZOOM_DISTANCE = 100; // change scale by this many arcseconds by default
+  private static int MAX_ZOOM_DISTANCE = 1080; // scale should not be greater than this.
+
+  int scale = MAX_ZOOM_DISTANCE; /* scale is number of seconds per pixel */
   /* origin is the co-ordinates of the origin of the view which is represented by MapPanel. */
-  int xorigin = 200;
-  int yorigin = -200;
+  int xorigin = 200; /* This panel's origin is xorigin number of pixels west of the prime meridian. */
+  int yorigin = -200; /* This panel's origin is yorigin number of pixels north of the equator. */
 
   public MapPanel() {
     super();
@@ -162,4 +166,58 @@ public class MapPanel extends JPanel {
   public int toSeconds(int pixels) {
     return pixels * scale;
   }
+
+  private void zoom(boolean in, int distance) {
+    if(in) {
+      if((scale - distance) >= 0) {
+        scale -= distance;
+      }
+    } else {
+      if((scale + distance) < MAX_ZOOM_DISTANCE) {
+        scale += distance;
+      }
+    }
+  }
+
+  private void zoom(boolean in) {
+    zoom(in, ZOOM_DISTANCE);
+  }
+
+  public void zoomIn() {
+    zoom(true);
+  }
+
+  public void zoomOut() {
+    zoom(false);
+  }
+
+  private void pan(char direction, int distance) {
+    switch(direction) {
+      case 'n': this.yorigin += distance; break;
+      case 's': this.yorigin -= distance; break;
+      case 'e': this.xorigin += distance; break;
+      case 'w': this.xorigin -= distance; break;
+    }
+  }
+
+  private void pan(char direction) {
+    pan(direction, scale * PAN_DISTANCE);
+  }
+
+  public void panN() {
+    pan('n');
+  }
+
+  public void panS() {
+    pan('s');
+  }
+
+  public void panE() {
+    pan('e');
+  }
+
+  public void panW() {
+    pan('w');
+  }
+
 }
